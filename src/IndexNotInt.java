ef1560c4486bf1a52fb00b360d09f22096f5aea7
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-04-25 19:38:29 +0100

    Forgot to add exception class

---
+++ src/IndexNotInt.java
@@ -0,0 +1,5 @@
+public class IndexNotInt extends SemanticException {
+    public IndexNotInt(SimpleNode simpleNode) {
+        super(simpleNode, "index type expected to be " + VarTypes.INT + " given " + simpleNode.jjtGetVal());
+    }
+}
