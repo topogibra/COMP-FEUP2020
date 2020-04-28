948f5ca624b815ad0a791e12bbdb8a2ff812ced6
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-04-28 18:12:03 +0100

    Assembler labels

---
+++ src/AssemblerLabels.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class AssemblerLabels {
+    private HashMap<String,Integer> map;
+
+    public AssemblerLabels() {
+        this.map = new HashMap<>();
+    }
+
+    public String getLabel(String label){
+        if(map.containsKey(label)){
+            int number = map.get(label);
+            map.put(label,number+1);
+            return label + "_" + number;
+        }else{
+            map.put(label,1);
+            return label+ "_" + 0;
+        }
+    }
+}
