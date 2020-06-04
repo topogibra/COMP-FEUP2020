91c40d69a18f433435aa470dff048cf800e6eaee
Gaspar Santos Pinheiro <up201704700@fe.up.pt>
2020-06-04 14:34:29 +0100

    Implemented compile function

--- test/jasmin/JmmCompiler.java
+++ test/jasmin/JmmCompiler.java
@@ -6,7 +6,8 @@ import pt.up.fe.specs.util.SpecsIo;
 
 public class JmmCompiler {
 	
-    private static String CLASS_WITH_MAIN = "Main";	
+    private static String CLASS_WITH_MAIN = "main.jmm";
+	private static String GENERATED_FILES_PATH = "jasminCode/";
 
 	
 	/**
@@ -20,10 +21,9 @@ public class JmmCompiler {
 		// This method needs to be implemented before the tests can run
 		// Below there is an example implementation, that assumes that the .j file is generated in the repository root
 		// Adapt the code according to your implementation.
-		throw new RuntimeException("Implement JmmCompiler.compile() in order to test the execution of .jmm files");			
+		//throw new RuntimeException("Implement JmmCompiler.compile() in order to test the execution of .jmm files");
 
 
-		/*
 		// Executes J-- compiler		
 		try {
             // Get class with main
@@ -41,18 +41,16 @@ public class JmmCompiler {
 			throw new RuntimeException("Error with compiling jmm", e);
 		}
 
-		var filename = SpecsIo.removeExtension(jmm.getName()) + ".j";
+		var filename = GENERATED_FILES_PATH + SpecsIo.removeExtension(jmm.getName()) + ".j";
 		
-		// TODO: where is the .j file? This code works when the .j file has the same name as the .jmm file, and is generated in the repository root. Adapt code according to how your J-- compiler works
 		var jFile = new File(filename);
 				
 		if(!jFile.isFile()) {
 			throw new RuntimeException("Could not find file " + jFile.getAbsolutePath());
 		}
 
-		return SpecsIo.read(jFile);		
-		*/
-				
+		return SpecsIo.read(jFile);
+
 	}
 	
 }
