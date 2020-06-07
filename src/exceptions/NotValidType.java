f0aa7c4198e5127a22917847b41593ea7a96e12b
Gaspar Santos Pinheiro <up201704700@fe.up.pt>
2020-06-07 18:50:29 +0100

    Updated README.txt

--- src/exceptions/NotValidType.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-import parser.SimpleNode;
-
-public class NotValidType extends SemanticException {
-    public NotValidType(SimpleNode simpleNode) {
-        super(simpleNode);
-    }
-}
